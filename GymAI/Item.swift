6378e7f966e1af830adc19f660e505e3f83c0e3a
Kamal Kishore <kamal.kishi@gmail.com>
2026-07-06 21:40:51 +0530

    Initial Commit

---
+++ GymAI/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GymAI
+//
+//  Created by Kamal Kishore on 06/07/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GymAITests/GymAITests.swift
@@ -0,0 +1,18 @@
+//
+//  GymAITests.swift
+//  GymAITests
+//
+//  Created by Kamal Kishore on 06/07/26.
+//
+
+import Testing
+
+struct GymAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
